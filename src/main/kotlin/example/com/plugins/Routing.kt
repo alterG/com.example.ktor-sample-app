89a22098ab75acc46c5785428276fb2103a587b6
Igor Shchipanov <oneyearday@gmail.com>
2024-08-01 19:26:28 +0300

    add static content

--- src/main/kotlin/example/com/plugins/Routing.kt
+++ src/main/kotlin/example/com/plugins/Routing.kt
@@ -2,11 +2,15 @@ package example.com.plugins
 
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
+
+        staticResources("/content", "content")
+
         get("/") {
             call.respondText("Hello World!")
         }
